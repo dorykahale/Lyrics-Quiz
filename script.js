3b0a832cb3720f5ede91d7ce9c805bf53da445b3
DMC <dorykahale@gmail.com>
2021-03-12 14:36:16 -0600

    Final Push

--- script.js
+++ script.js
@@ -1,172 +1,206 @@
-const startButton = document.getElementById("start-btn")
-const nextButton = document.getElementById("next-btn")
-const questionContainerElement = document.getElementById("question-container")
-const questionElement = document.getElementById("question")
-const answerButtonsElement = document.getElementById("answer-buttons")
-const timeEl = document.getElementById("time");
-
-let shuffledQuestions, currentQuestionIndex
-
-function setTime() {
-    const timeEl = document.getElementById("time")
-    secondsLeft = 60
-    const timerInterval = setInterval(function() {
-    secondsLeft--
-    timeEl.textContent = secondsLeft
-    
-
-    if(secondsLeft === 0) {
-      clearInterval(timerInterval)
-      showScore();
-    }
-
-  }, 1000);
-}
-
-  
-startButton.addEventListener("click", startGame)
-nextButton.addEventListener("click", () => {
-    currentQuestionIndex++
-    setNextQuestion()
-})
-function startGame() {
-    setTime()
-    startButton.classList.add("hide")
-    shuffledQuestions = questions.sort(() => Math.random() - .5)
-    currentQuestionIndex = 0
-    questionContainerElement.classList.remove("hide")
-    setNextQuestion()
-}
-
-function setNextQuestion() {
-    resetState()
-    showQuestion(shuffledQuestions[currentQuestionIndex])
-}
-
-function showQuestion(question) {
-    questionElement.innerText = (question.question)
-    question.answers.forEach(answer => {
-        const button = document.createElement("button")
-        button.innerText = answer.text
-        button.classList.add("btn")
-        if (answer.correct) {
-            button.dataset.correct = answer.correct
-        }
-        button.addEventListener("click", selectAnswer)
-        answerButtonsElement.appendChild(button)
-    })
-}
-
-function resetState() {
-    clearStatusClass(document.body)
-    nextButton.classList.add("hide")
-    while (answerButtonsElement.firstChild) {
-        answerButtonsElement.removeChild(answerButtonsElement.firstChild)
-    }
-}
-
-function selectAnswer(e) {
-    const selectedButton = e.target
-    const correct = selectedButton.dataset.correct
-    setStatusClass(document.body, correct)
-    Array.from(answerButtonsElement.children).forEach(button => {
-        setStatusClass(button, button.dataset.correct)
-    })
-
-    if (shuffledQuestions.length > currentQuestionIndex +1) {
-        nextButton.classList.remove("hide")
-    } else {
-        startButton.innerText = "Restart"
-        startButton.classList.remove("hide")
-    }   
-}
-
-function setStatusClass(element, correct) {
-    clearStatusClass(element)
-    if (correct) {
-        element.classList.add("correct") 
-    } else {
-        element.classList.add("wrong")
-    }
-}
-
-function clearStatusClass(element) {
-element.classList.remove("correct")
-element.classList.remove("wrong")
-}
-
-const questions = [
+var questions = [
 
     {
-        question: "All You Need is ____.",
-        answers: [
-            { text: "Love", correct: true},
-            { text: "Fiber", correct: false},
-            { text: "Cash", correct: false},
-            { text: "Netflix", correct: false}
-        ]
-        
+        question: "All You Need Is ____.",
+        choices: ["Fiber", "Cash", "Love", "Netflix"],
+        answer: "Love"
     },
-    {   
-        question: "Fight For Your ____ To Party.",
-        answers: [
-            { text: "Life", correct: false},
-            { text: "Right", correct: true},
-            { text: "House", correct: false},
-            { text: "Pets", correct: false}
-        ]
-    },    
-    {   
-        question: "Now, I ain't sayin' she a ____ ____.",
-        answers: [
-            { text: "Life Partner", correct: false},
-            { text: "Happy Camper", correct: false},
-            { text: "Gold Digger", correct: true},
-            { text: "Life Guard", correct: false}
-        ]
+    {
+        question: "Fight For Your____To Party",
+        choices: ["Life", "Right", "House", "Pets"],
+        answer: "Right"
     },
-    {   
-        question: "I love it when you call me ___ ___.",
-        answers: [
-            { text: "Love Doctor", correct: false},
-            { text: "Gold Digger", correct: false},
-            { text: "John Doe", correct: false},
-            { text: "Big Poppa", correct: true}
-        ]
-    }    
+    {
+        question: "Now, I Ain't Sayin' She A ____ ____",
+        choices: ["Life Partner", "Happy Camper", "Gold Digger", "Life Guard"],
+        answer: "Gold Digger"
+    },
+    {
+        question: "I Love It When You Call Me ____ ____",
+        choices: ["Love Doctor", "Gold Digger", "John Doe", "Big Poppa"],
+        answer: "Big Poppa"
+    },
+    {
+        question: "Is This The ____ ____? Is This Just Fantasy",
+        choices: ["Last Stop", "Real Life", "Poor Boy", "Right Place"],
+        answer: "Real Life"
+    },
+    {
+        question: "See That Girl. ____ ____ ____. Digging The Dancing Queen",
+        choices: ["Keep Her Dream", "Watch That Scene", "Make It Clean", "Dance It Clean"],
+        answer: "Watch That Scene"
+    },
+    {
+        question: "Blinded By The Light. Revved Up Like A ____ ____ ____ In The Night",
+        choices: ["Fuse And Execution", "Deuce Another Runner", "Fool With Obligation", "Douche Another Rumor"],
+        answer: "Deuce Another Runner"
+    },
 
 ]
+var score = 0;
+var questionIndex = 0;
 
-// Function to create and append score to score.html
-var storedScores = JSON.parse(localStorage.getItem("userData"));
-var highScoresArea = document.querySelector("#highScoresList");
-var backBtn = document.querySelector("#backButton");
-var clearBtn = document.querySelector("#clearScores");
+var currentTime = document.querySelector("#currentTime");
+var timer = document.querySelector("#startTime");
+var questionsDiv = document.querySelector("#questionsDiv");
+var wrapper = document.querySelector("#wrapper");
 
 
-function displayScores() {
-    if (storedScores !== null) {
-        var scoreList = document.createElement("ol");
-        scoreList.className = "scoreListClass";
-        for (var i = 0; i < storedScores.length; i++) {
-            var initials = storedScores[i].inits;
-            var scores = storedScores[i].userScore
-            var scoreEntry = document.createElement("li");
-            scoreEntry.innerHTML = initials + " - " + scores;
-            scoreList.appendChild(scoreEntry);
-        }
-        highScoresArea.appendChild(scoreList);
+var secondsLeft = 61;
+var holdInterval = 0;
+var penalty = 5;
+
+var ulCreate = document.createElement("ul");
+
+
+timer.addEventListener("click", function () {
+
+    if (holdInterval === 0) {
+        holdInterval = setInterval(function () {
+            secondsLeft--;
+            currentTime.textContent = "Time: " + secondsLeft;
+
+            if (secondsLeft <= 0) {
+                clearInterval(holdInterval);
+                allDone();
+                currentTime.textContent = "Time's up!";
+            }
+        }, 1000);
     }
-};
-
-displayScores();
-
-backBtn.addEventListener("click", function () {
-    location.href = "index.html";
+    render(questionIndex);
 });
 
-clearBtn.addEventListener("click", function () {
-    highScoresArea.innerHTML = "";
-    window.localStorage.clear();
+// Questions and choices
+function render(questionIndex) {
 
-});
+    questionsDiv.innerHTML = "";
+    ulCreate.innerHTML = "";
+
+    for (var i = 0; i < questions.length; i++) {
+
+        var userQuestion = questions[questionIndex].question;
+        var userChoices = questions[questionIndex].choices;
+        questionsDiv.textContent = userQuestion;
+    }
+
+    userChoices.forEach(function (newItem) {
+        var listItem = document.createElement("li");
+        listItem.textContent = newItem;
+        questionsDiv.appendChild(ulCreate);
+        ulCreate.appendChild(listItem);
+        listItem.addEventListener("click", (compare));
+    })
+}
+// Compares choices with answer
+function compare(event) {
+    var element = event.target;
+
+    if (element.matches("li")) {
+
+        var createDiv = document.createElement("div");
+        createDiv.setAttribute("id", "createDiv");
+        // Correct answer
+        if (element.textContent == questions[questionIndex].answer) {
+            score++;
+            createDiv.textContent = "Correct! The answer is:  " + questions[questionIndex].answer;
+            // Correct answer
+        } else {
+            // Deducts -7 seconds for wrong answer
+            secondsLeft = secondsLeft - penalty;
+            createDiv.textContent = "Wrong! The correct answer is:  " + questions[questionIndex].answer;
+        }
+
+    }
+
+    questionIndex++;
+
+    if (questionIndex >= questions.length) {
+        // Appends last page with user stats
+        allDone();
+        createDiv.textContent = "End of quiz!" + " " + "You got  " + score + "/" + questions.length + " Correct!";
+    } else {
+        render(questionIndex);
+    }
+    questionsDiv.appendChild(createDiv);
+
+}
+
+function allDone() {
+    questionsDiv.innerHTML = "";
+    currentTime.innerHTML = "";
+
+    // Heading:
+    var createH1 = document.createElement("h1");
+    createH1.setAttribute("id", "createH1");
+    createH1.textContent = "All Done!"
+
+    questionsDiv.appendChild(createH1);
+
+    // Paragraph
+    var createP = document.createElement("p");
+    createP.setAttribute("id", "createP");
+
+    questionsDiv.appendChild(createP);
+
+    // Final Score
+    if (secondsLeft >= 0) {
+        var timeRemaining = secondsLeft;
+        var createP2 = document.createElement("p");
+        clearInterval(holdInterval);
+        createP.textContent = "Your final score is: " + timeRemaining;
+
+        questionsDiv.appendChild(createP2);
+    }
+
+    // Label
+    var createLabel = document.createElement("label");
+    createLabel.setAttribute("id", "createLabel");
+    createLabel.textContent = "Enter your initials: ";
+
+    questionsDiv.appendChild(createLabel);
+
+    // Input
+    var createInput = document.createElement("input");
+    createInput.setAttribute("type", "text");
+    createInput.setAttribute("id", "initials");
+    createInput.textContent = "";
+
+    questionsDiv.appendChild(createInput);
+
+    // Submit
+    var createSubmit = document.createElement("button");
+    createSubmit.setAttribute("type", "submit");
+    createSubmit.setAttribute("id", "Submit");
+    createSubmit.textContent = "Submit";
+
+    questionsDiv.appendChild(createSubmit);
+
+    // Captures initials and local storage
+    createSubmit.addEventListener("click", function () {
+        var initials = createInput.value;
+
+        if (initials === null) {
+
+            console.log("No value entered!");
+
+        } else {
+            var finalScore = {
+                initials: initials,
+                score: timeRemaining
+            }
+            console.log(finalScore);
+            var allScores = localStorage.getItem("allScores");
+            if (allScores === null) {
+                allScores = [];
+            } else {
+                allScores = JSON.parse(allScores);
+            }
+            allScores.push(finalScore);
+            var newScore = JSON.stringify(allScores);
+            localStorage.setItem("allScores", newScore);
+
+            window.location.replace("./scores.html");
+        }
+    });
+
+}
